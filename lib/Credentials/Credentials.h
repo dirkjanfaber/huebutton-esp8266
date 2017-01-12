a7509e5971da438516a896abf3e6995c9d8a6baa
Dirk-Jan Faber <d.faber@gmail.com>
2017-01-12 12:22:56 +0100

    Boilerplate credentials file for connecting to both wifi as hue

---
+++ lib/Credentials/Credentials.h
@@ -0,0 +1,14 @@
+// Wifi credentials, please change this to your wifi network
+#define WIFI "wifissd"
+#define WIFIPW "s3cr3t"
+
+// The HUE info
+#define HUE_BRIDGE "192.168.2.2"
+#define API_KEY "hue-api-key"
+// Which light to switch
+#define LIGHT "2"
+// The color to switch to (in this case non bright pink)
+#define HUE 57638
+#define BRI 30
+#define SAT 254
+
